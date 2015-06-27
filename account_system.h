9577ad08954e2246d208b923519878abd93bef6b
Chiang, Yi-Yo <big2632@gmail.com>
2015-06-27 17:40:03 +0800

    50 percent

--- account_system.h
+++ account_system.h
@@ -2,15 +2,19 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
+#include <tuple>
 
 class TransferRecord{
   public:
     enum Type {Deposit, Withdraw};
+    int timeStamp;
     Type type;
     int hashID;
     long long money;
-    TransferRecord(Type type, int hashID, long long money) : 
-      type(type), hashID(hashID), money(money) {}
+    TransferRecord() {}
+    TransferRecord(int timeStamp, Type type, int hashID, long long money) : 
+      timeStamp(timeStamp), type(type), hashID(hashID), money(money) {}
 };
 
 class Account{
@@ -24,26 +28,42 @@ class Account{
       ID(ID), hashID(hashID), hashPWD(hashPWD), balance(balance), records() {}
 };
 
+enum Status {IDNotFound, WrongPassword, Success, Fail};
+
 class AccountSystem{
   public:
     int timeStamp;
-    int currentLoginHashID;
+    int unusedHashID;
+    int lastLoginHashID;
 
-    std::set<std::string> IDs;  // set of used IDs.
+    std::set<std::string> IDs;                // set of used IDs.
     std::map<std::string, int> __toHashID;
     std::vector<std::string> __fromHashID;
+    std::vector<int> __parent;          // Use disjoint-set to maintain.
     std::vector<Account> accounts;
     
     // It doesn't check if ID exists or not.
     int toHashID(const std::string& ID) const
     {
-      return __toHashID[ID];
+      return __toHashID.at(ID);
+    }
+
+    int find(int hashID)
+    {
+      return __parent[hashID] == hashID ? hashID : __parent[hashID] = find(__parent[hashID]);
+    }
+
+    void merge(int hashID1, int hashID2)
+    {
+      hashID1 = find(hashID1);
+      hashID2 = find(hashID2);
+      __parent[hashID2] = hashID1;
     }
 
     // This one doesn't check, either;
-    const std::string& fromHashID(int hashID) const
+    const std::string& fromHashID(int hashID)
     {
-      return __fromHashID[hashID];
+      return __fromHashID[find(hashID)];
     }
     
     bool exist(const std::string& ID) const
@@ -52,17 +72,77 @@ class AccountSystem{
     }
 
     // All passwords should be hashed before calling functions.
-    bool create(const std::string& ID, const std::string& hashPWD){
-      if(exist(ID)) return false;
-      int hashID = timeStamp++;
+    Status create(const std::string& ID, const std::string& hashPWD)
+    {
+      if(exist(ID)) return Fail;
+      int hashID = unusedHashID++;
       accounts.emplace_back(ID, hashID, hashPWD, 0);
       IDs.insert(ID);
       __toHashID[ID] = hashID;
       __fromHashID.push_back(ID);
-      return true;
+      __parent.push_back(hashID);
+      return Success;
     }
 
-    bool 
+    Status login(const std::string& ID, const std::string& hashPWD)
+    {
+      if(!exist(ID)) return IDNotFound;
+      int hashID = toHashID(ID);
+      if(hashPWD != accounts[hashID].hashPWD){
+        return WrongPassword;
+      }
+      lastLoginHashID = hashID;
+      return Success;
+    }
+    
+    void __destroy(const std::string& ID)
+    {
+      IDs.erase(ID);
+      __toHashID.erase(ID);
+    }
 
-    AccountSystem() : timeStamp(0), currentLoginHashID(0) {}
+    Status destroy(const std::string& ID, const std::string& hashPWD)
+    {
+      if(!exist(ID)) return IDNotFound;
+      int hashID = toHashID(ID);
+      Account &account = accounts[hashID];
+      if(hashPWD != account.hashPWD) return WrongPassword;
+      __destroy(ID);
+      return Success;
+    }
+
+    std::tuple<Status, long long> merge(const std::string& ID1, const std::string& hashPWD1
+                                      , const std::string& ID2, const std::string& hashPWD2)
+    {
+      if(!exist(ID1)) return std::make_tuple(IDNotFound, 0);
+      if(!exist(ID2)) return std::make_tuple(IDNotFound, 1);
+      int hashID1 = toHashID(ID1);
+      int hashID2 = toHashID(ID2);
+      Account &account1 = accounts[hashID1], &account2 = accounts[hashID2];
+      if(hashPWD1 != account1.hashPWD) return std::make_tuple(WrongPassword, 0);
+      if(hashPWD2 != account2.hashPWD) return std::make_tuple(WrongPassword, 1);
+      account1.balance += account2.balance;
+      merge(hashID1, hashID2);
+      std::vector<TransferRecord> records(account1.records.size() + account2.records.size());
+      auto it1 = account1.records.begin(), it2 = account2.records.begin();
+      auto end1 = account1.records.end(), end2 = account2.records.end();
+      for(auto &record : records){
+        if(it1 == end1){
+          record = *it2++;
+        }else if(it2 == end2){
+          record = *it1++;
+        }else{
+          if(it1->timeStamp <= it2->timeStamp){
+            record = *it1++;
+          }else{
+            record = *it2++;
+          }
+        }
+      }
+      account1.records = records;
+      __destroy(ID2);
+      return std::make_tuple(Success, account1.balance);
+    }
+
+    AccountSystem() : timeStamp(0), unusedHashID(0), lastLoginHashID(0) {}
 };
